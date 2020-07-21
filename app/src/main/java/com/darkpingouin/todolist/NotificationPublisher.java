67163fb2bf649ef4469e7368c1c75e19178872c2
quantvhq99 <quantvhq99@gmail.com>
2020-07-21 23:35:16 +0700

    Ui update edit_item

--- app/src/main/java/com/darkpingouin/todolist/NotificationPublisher.java
+++ app/src/main/java/com/darkpingouin/todolist/NotificationPublisher.java
@@ -22,5 +22,6 @@ public class NotificationPublisher extends BroadcastReceiver {
         Notification notification = intent.getParcelableExtra(NOTIFICATION);
         int id = intent.getIntExtra(NOTIFICATION_ID, 0);
         notificationManager.notify(id, notification);
+        //abc
     }
 }
